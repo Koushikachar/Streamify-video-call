f9ced8655d6c4e2bf69f00aa474910324a8f071f
Koushikachar <Koushikachar25@gmail.com>
2025-11-30 10:34:34 +0530

    done

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const BASEURL =
-  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+  import.meta.env.MODE === "development"
+    ? "https://streamify-video-calling-six.vercel.app/api"
+    : "/api";
 export const axiosInstance = axios.create({
   baseURL: BASEURL,
   withCredentials: true,
